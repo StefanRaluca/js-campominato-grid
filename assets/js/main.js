5f837adcaeca5d0f643c8a24bfbf8bef9b5c5728
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-28 15:48:20 +0100

    add row and column with cells at container list with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,38 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 
 let grid = document.getElementById('container_list')
 console.log(grid);
-let play = document.querySelector("button");
-console.log(play);
+let playButton = document.querySelector("button");
+console.log(playButton);
 
 // formiamo le rows and column al interno del div container list 
 
-for (let i = 0; i < column; i++) {
-    const columns = column[i];
-    
+// facciamo una funzione in quale generiamo le righe e le collone 
+function listContainer() {
+
+    // Generiamo le righe del container list
+    for (let i = 0; i < 10; i++) {
+        // Creiamo un elemento div per rappresentare la riga
+        let row = document.createElement('div');
+        row.classList.add('container-row');
+        console.log(row);
+
+        // Aggiungiamo la riga al container della griglia
+        grid.appendChild(row);
+
+        // Generiamo le colonne della riga 
+        for (let y = 0; y < 10; y++) {
+            // facciamo aggiungere un elemento div per ogni cubo
+            let column = document.createElement('div');
+            column.classList.add('container-column');
+            console.log(column);
+            // Assegniamo un numero progressivo alla cella
+            let progressiveNumber = i * 10 + y + 1;
+            // mettiamo nella cella il numero progressivo che lo abbiamo definito prima nella let
+            column.innerHTML = progressiveNumber;
+            console.log(progressiveNumber);
+
+            // Mettiamo alla riga la rispetiva cella 
+            row.appendChild(column);
+        }
+    }
 }
